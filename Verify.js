ffab796c5eef25cdba6842d75b49a7a7614e97c8
Sahil Gupta <gupsahil2005@gmail.com>
2023-07-30 14:06:03 +0530

    third commit

--- Verify.js
+++ Verify.js
@@ -1,12 +1,17 @@
 import jwt from "jsonwebtoken";
-import  {createError}  from "./error.js";
+export const ErrorMessage = (status,message)=>{
+  const error = new Error();
+  error.status = status;
+  error.message = message;
+  return error;
+}
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-    if (!token) return next(createError(401, "You are not authenticated!"));
+    if (!token) return next(ErrorMessage(401, "You are not authenticated!"));
   
     jwt.verify(token, process.env.JWT, (err, user) => {
-      if (err) return next(createError(403, "Token is not valid!"));
+      if (err) return next(ErrorMessage(403, "Token is not valid!"));
       req.user = user;
       next()
     });
